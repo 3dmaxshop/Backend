804af7b63982f6ebb0f03fed6a015691ba609e60
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-06-10 01:21:09 +0300

    delete function and fix index error from add function

--- server.py
+++ server.py
@@ -19,9 +19,24 @@ def check_model_id_in_unique(model_id, models):
             return model
     return False
 
+def delete_model_id_in_unique(model_id, models):
+    for model in models:
+        if model['id'] == model_id:
+            models.remove(model)
+    return None
+
+#DELETE /api/v1/models/1 - удаляет модельь с ид. 1
+
+@app.delete('/api/v1/models/<int:model_id>')
+def delete_model_from_id(model_id):
+    model = check_model_id_in_unique(model_id, models)
+    if model == False:
+        return json.dumps([{'Error': f'Модель с id: {model_id} не найдена в базе'}])
+    delete_model_id_in_unique(model_id, models)
+    return json.dumps([{'result': f'Модель с id: {model_id} удалена'}])
+
 @app.get('/api/v1/models/<int:model_id>')
 def get_model_from_id(model_id):
-    id_model = model_id
     model = check_model_id_in_unique(model_id, models)
     if model == False:
         return json.dumps([{'Error': f'Модель с id: {model_id} не найдена в базе'}])
@@ -36,7 +51,10 @@ def add_model():
     payload = request.json
     if not check_model_name_in_unique(payload, models):
         return json.dumps([{"Error": "Модель с таким именем уже есть в базе"}])
-    payload['id'] = models[-1]['id'] + 1
+    if len(models) == 0:
+         payload['id'] = 0
+    else:
+        payload['id'] = models[-1]['id'] + 1
     models.append(payload)
     return payload
     
