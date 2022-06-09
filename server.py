2aa8c49b95716da03e9a7bed2e8c579b8d2fa679
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-06-10 01:00:56 +0300

    get_model_from_id; add last id + 1 когда добавляется новая модель

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
+from pyexpat import model
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
-models = [{'name': 'sofa1','color': 'blue','catigories': 'sofa',},
-           {'name': 'sofa2','color': 'red','catigories': 'sofa',}]
+models = [{'id': 0, 'name': 'sofa1','color': 'blue','catigories': 'sofa',},
+           {'id': 1,'name': 'sofa2','color': 'red','catigories': 'sofa',}]
 
 def check_model_name_in_unique(payload, models):
     for model in models:
@@ -12,6 +13,23 @@ def check_model_name_in_unique(payload, models):
             return False
     return True
 
+def check_model_id_in_unique(model_id, models):
+    for model in models:
+        if model['id'] == model_id:
+            return model
+    return False
+
+zero_model_id = 0
+
+#GET /api/v1/models/1 - получить модель с ид. 1
+
+@app.get('/api/v1/models/<int:model_id>')
+def get_model_from_id(model_id):
+    id_model = model_id
+    model = check_model_id_in_unique(model_id, models)
+    if model == False:
+        return json.dumps([{'Error': f'Модель с id: {model_id} не найдена в базе'}])
+    return json.dumps(model)
 
 @app.get('/api/v1/models/')
 def get_models():
@@ -22,6 +40,7 @@ def add_model():
     payload = request.json
     if not check_model_name_in_unique(payload, models):
         return json.dumps([{"Error": "Модель с таким именем уже есть в базе"}])
+    payload['id'] = models[-1]['id'] + 1
     models.append(payload)
     return payload
     
