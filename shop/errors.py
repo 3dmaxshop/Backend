6ff22feb972ceaebdf4c9fdae60cc97a51710a08
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-06-14 22:38:58 +0300

    add errors class

---
+++ shop/errors.py
@@ -0,0 +1,26 @@
+from telnetlib import STATUS
+
+
+class AppError (Exception):
+    def __init__(self, reason:str, status: int) -> None:
+        super().__init__(f'[{status}] {reason}')
+        self.reason = reason
+        self.status = status
+
+class Conflict(AppError):
+
+    status = 409
+
+    def __init__(self, entity: str, info:str) -> None:
+        super().__init__(reason=f'[{entity}] conflict {info}', status = self.status)
+        self.entity = entity
+        self.info = info
+
+class NotFoundError(AppError):
+
+    status = 404
+    
+    def __init__(self, entity: str, info:str) -> None:
+        super().__init__(reason=f'[{entity}] conflict {info}', status = self.status)
+        self.entity = entity
+        self.info = info
