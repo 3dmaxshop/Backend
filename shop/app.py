c96d671e9ce6b65b2cf2b71512c106a6270e1829
Dmitriy Volynkin <volynkin-da@yandex.ru>
2022-06-21 09:33:21 +0300

    fix

--- shop/app.py
+++ shop/app.py
@@ -15,13 +15,12 @@ def handle_app_errors(error: AppError):
     return err
 
 
-
 def handle_validation_errors(error: ValidationError):
     logger.warning(str(error))
     return {'error': error.errors()}, 422
 
+
 app = Flask(__name__)
 app.register_error_handler(AppError, handle_app_errors)
-flask_app.register_error_handler(ValidationError, handle_validation_errors)
+app.register_error_handler(ValidationError, handle_validation_errors)
 app.register_blueprint(models.routes, url_prefix='/api/v1/models')
-
